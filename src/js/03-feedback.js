83337f131fc1cc88e1ff2bd9fc07d14abc5bc338
Antonyly <97746965+Antonyly@users.noreply.github.com>
2022-05-29 12:59:37 +0300

    Initial commit

---
+++ src/js/03-feedback.js
@@ -0,0 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+  const email = document.querySelector('input');
+  const message = document.querySelector('textarea');
+
+const formData = {};
+
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  
+}
+
+ function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (!data) {
+    email.value = '';
+    message.value = '';
+  }
+  // if (!data) {
+  //   return;
+  // }
+};
+dataFromLocalStorage();
