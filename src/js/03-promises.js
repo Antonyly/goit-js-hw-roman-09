50c6fbc61503c787be803db95393d19694802f22
Antonyly <antonlyzun@gmail.com>
2022-05-31 19:30:05 +0300

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = { position, delay };
+    return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(promise);
+    } else {
+      reject(promise);
+    }
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  const countStep = Number(e.currentTarget.step.value);
+  const amountValue = Number(e.currentTarget.amount.value);
+
+  for (let position = 1; position <= amountValue; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`,);
+        }, delay);
+      });
+    delay += countStep;
   }
 }
