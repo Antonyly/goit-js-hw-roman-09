5c23246a71a367ceac6f681b9451c028e4a51014
Antonyly <antonlyzun@gmail.com>
2022-06-01 08:41:31 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -26,12 +26,12 @@ function submitForm(e) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         setTimeout(() => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
         }, delay);
       })
       .catch(({ position, delay }) => {
         setTimeout(() => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`,);
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
         }, delay);
       });
     delay += countStep;
